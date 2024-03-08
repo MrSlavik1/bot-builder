8c0193be3fe4f2434c0d0cf1e3c19f158b11cbc6
MrSlavik1 <70208281+MrSlavik1@users.noreply.github.com>
2024-03-09 01:20:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -101,8 +101,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.query_id}`;
 
 usercard.appendChild(p);
 
